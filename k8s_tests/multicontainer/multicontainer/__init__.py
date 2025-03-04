abc9b1d77583fac0781a0c92958dd0d73be49959
Manuel Gil <manuelgilsitio@gmail.com>
2025-03-03 22:48:50 -0500

    multicontainer pod

---
+++ k8s_tests/multicontainer/multicontainer/__init__.py
@@ -0,0 +1,38 @@
+from fastapi import FastAPI
+from fastapi.responses import JSONResponse
+from fastapi import status
+import os
+import mlflow
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    print("MLFLOW_TRACKING_URI:", os.environ.get("MLFLOW_TRACKING_URI", None))
+    
+    return {"Hello": "World"}
+
+@app.get("/list_experiments")
+def get_experiment(experiment_name: str = "default"):
+    tracking_uri = os.environ.get("MLFLOW_TRACKING_URI", None)
+    if tracking_uri is None:
+        return JSONResponse(
+            content={"error": "MLFLOW_TRACKING_URI environment variable not set."},
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+        )
+    print("MLFLOW_TRACKING_URI:", tracking_uri)
+    mlflow.set_tracking_uri(tracking_uri)
+
+    experiment = mlflow.get_experiment_by_name(experiment_name)
+    if experiment is None:
+        return JSONResponse(
+            content={"error": f"Experiment {experiment_name} not found."},
+            status_code=status.HTTP_404_NOT_FOUND,
+        )
+
+    return JSONResponse(
+        content={
+            "experiment_id": experiment.experiment_id,
+            "experiment_name": experiment.name,
+        }
+    )
