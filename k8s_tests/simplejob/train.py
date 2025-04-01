5c2fc93434074a27865731a33526c17502e1ee7c
Manuel Gil <manuelgilsitio@gmail.com>
2025-03-31 21:01:37 -0500

    job + mlflow

--- k8s_tests/simplejob/train.py
+++ k8s_tests/simplejob/train.py
@@ -5,7 +5,8 @@ from sklearn.ensemble import RandomForestClassifier
 import joblib
 import pandas as pd
 import mlflow 
-
+import os 
+import time
 # Generate a synthetic dataset
 def generate_data(n_samples=1000, n_features=20, n_classes=2):
     X, y = make_classification(n_samples=n_samples, n_features=n_features, n_classes=n_classes, random_state=42)
@@ -17,11 +18,11 @@ def train_model(X_train, y_train):
     model.fit(X_train, y_train)
     return model
 
-# Save the model to a file
-def save_model(model, filename='model.pkl'):
-    joblib.dump(model, filename)
+# # Save the model to a file
+# def save_model(model, filename='model.pkl'):
+#     joblib.dump(model, filename)
 
-def train(experiment):
+def train():
     # Generate data
     X, y = generate_data()
 
@@ -31,7 +32,7 @@ def train(experiment):
     # Train the model
     model = train_model(X_train, y_train)
 
-    with mlflow.start_run(experiment_id = experiment.experiment_id) as run:
+    with mlflow.start_run() as run:
         # Log the model parameters and metrics
         mlflow.log_param("n_estimators", 100)
         mlflow.log_metric("accuracy", accuracy_score(y_test, model.predict(X_test)))
@@ -48,14 +49,20 @@ def train(experiment):
     print(f'Model accuracy: {accuracy:.2f}')
 
 if __name__ == "__main__":
-    import os 
-    import time
     service_port = os.environ.get("MLFLOW_SERVICE_SERVICE_PORT")
     service_host = os.environ.get("MLFLOW_SERVICE_SERVICE_HOST")
     print("MLflow Service Host and Port:")
     print(service_port, service_host)
-    MLFLOW_TRACKING_URI = f"http://{service_host}:{service_port}"
-    os.environ["MLFLOW_TRACKING_URI"] = MLFLOW_TRACKING_URI  # Set the MLflow tracking URI in the environment variable
-    time.sleep(10)  # Wait for the MLflow server to start
+    # MLFLOW_TRACKING_URI = f"http://{service_host}:{service_port}"
+    MLFLOW_TRACKING_URI = os.environ.get("MLFLOW_TRACKING_URI")  # Replace with your MLflow server URI
+    print("MLflow Tracking URI:")
+    print(MLFLOW_TRACKING_URI)    
+    mlflow.set_tracking_uri(MLFLOW_TRACKING_URI)  # Set the MLflow tracking URI
     experiment = mlflow.set_experiment("RandomForest_Experiment")  # Set the experiment name
-    train(experiment)
+    print("Experiment ID:")
+    print(experiment.experiment_id)
+    print("Experiment Name:")
+    print(experiment.name)
+    tracking_uri = mlflow.get_tracking_uri()
+    print(f"Current tracking uri: {tracking_uri}")
+    train()
