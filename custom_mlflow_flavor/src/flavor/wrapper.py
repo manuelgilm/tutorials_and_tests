a795c5b7a0b392b29efce1513035f570e5abc339
Manuel Gil <manuelgilsitio@gmail.com>
2025-09-28 15:41:30 -0500

    update

--- custom_mlflow_flavor/src/flavor/wrapper.py
+++
@@ -1,9 +0,0 @@
-from src.base import Circle
-
-class CircleWrapper:
-
-    def __init__(self, circle:Circle) -> None:
-        self.circle = circle
-
-    def predict(self, x: float, y: float) -> bool:
-        return self.circle.predict(x, y)
